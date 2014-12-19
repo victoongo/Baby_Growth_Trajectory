391c95c8bdeeab4586ec20e04acb829a5488fc60
Victor Wang <victoongo@gmail.com>
2014-12-19 13:02:27 -0500

    rnw for bmi analysis using sitar

--- bmi.R
+++ bmi.R
@@ -1,6 +1,7 @@
 library(sitar)
 library(ggplot2)
 library(plyr)
+library(nlme)
 
 setwd("~/proj_data/bmi")
 bmi<-read.csv("bmi.csv", as.is=TRUE)
@@ -13,6 +14,9 @@ bmi <- unique(bmi)
 bmi$id <- bmi$mom_nestid * 100000 + bmi$nestid
 
 ggplot(aes(x=agemos), data=bmi) + geom_histogram() 
+bmi60 <- bmi[bmi$agemos<=60,]
+qplot(agemos, height, data=bmi60, geom=c("point","smooth"), method="gam", formula=y~ns(x,3), 
+      size=weight, alpha=I(1/3), colour=factor(sex)) 
 age <- bmi[bmi$agemos<0.5,]
 ggplot(aes(x=agemos), data=age) + geom_histogram() 
 
@@ -53,7 +57,7 @@ mplot(agemos, haz, id, bmi)
 plotclean(agemos, weight, id, bmi)
 
 waz <- bmi[!is.na(bmi$height),]
-waz <- subset(waz, select=c(height, agemos, id, ref_date, ht_source))
+waz <- subset(waz, select=c(height, agemos, id, ref_date, ht_source, sex))
 waz <- unique(waz)
 
 minmax <- ddply(waz, .(id), summarize, agemos_min=min(agemos), agemos_max=max(agemos))
@@ -134,3 +138,39 @@ nwaz <- zapvelout(outliers, icode=6)
 
 length(unique(epi$nestid)) == nrow(epi)
 rownames(epi) <-epi$nestid
+
+
+
+
+# nlme model
+lm <- lme(height ~ agemos, data=nwaz, random= ~ agemos | id, method="ML")
+qm <- lme(height ~ agemos + I(agemos^2), data=nwaz, random= ~ agemos | id, method="ML")
+cm <- lme(height ~ agemos + I(agemos^2) + I(agemos^3), data=nwaz, random= ~ agemos | id, method="ML")
+plot(lm, y2par=list(col='blue', apv=TRUE))
+plot(qm, y2par=list(col='blue', apv=TRUE))
+plot(cm, y2par=list(col='blue', apv=TRUE))
+plot(qm, fitted(.) ~ agemos)
+pwaz <- nwaz
+pwaz$pred <- predict(cm, newdata=pwaz,level=0)
+ggplot(pwaz,aes(x=agemos,y=height,color=as.factor(id)),alpha=I(1/3)) + 
+  geom_path() +
+  geom_line(data=pwaz,aes(x=agemos,y=pred),color="black") +
+  scale_y_continuous("height")
+plot(nwaz$agemos, nwaz$height, type='l')
+ggplot(pwaz,aes(x=agemos,y=height)) + geom_line() 
+qplot(agemos, height, data=pwaz, geom="path", colour=as.factor(id), alpha=I(1/2)) + scale_size_area()
+anova(lm, qm)
+anova(qm, cm)
+
+qm_sex <- lme(height ~ agemos + I(agemos^2) + sex, data=nwaz, random= ~ agemos + I(agemos^2) | id, method="ML")
+cm_sex <- lme(height ~ agemos + I(agemos^2) + I(agemos^3) + sex, data=nwaz, random= ~ agemos | id, method="ML")
+pwaz_sex <- nwaz
+pwaz_sex$pred <- predict(qm_sex, newdata=pwaz_sex,level=0)
+ggplot(pwaz, aes(agemos, height, group=id, colour=factor(id)), alpha=I(1/10)) + geom_line() +
+  geom_smooth(aes(group=id), methos="lm", size=0.2, colour="blue", se=F) +
+  geom_smooth(aes(group=1), method="lm", formula=y~ns(x,3), size=2, se=F, colour="red")
+
+ggplot(pwaz_sex,aes(x=agemos,y=height,color=factor(sex)), alpha=I(1/3)) + 
+  geom_path() +
+  geom_line(data=pwaz_sex,aes(x=agemos,y=pred,group=sex,color=factor(sex)), colour="black") +
+  scale_y_continuous("height")
